9d0dc86be9b74256de3b423e6650a40d846b8974
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-30 17:07:19 +0100

    Ajouter la methode de chragement du fichier JSON

--- app/src/main/java/com/example/ex4json202/MainActivity.java
+++ app/src/main/java/com/example/ex4json202/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public String LoadJsonFromRaw(int resId){
+        try {
+            InputStream in = getResources().openRawResource(resId);
+            byte[] data = new byte[in.available()];
+            in.read(data);
+            return new String(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
 }
